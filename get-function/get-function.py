232e130b6b391a8759c43f5fcd23300de3eaae39
Wilkin Ruiz <wilkin.ruizvalerio@gmail.com>
2024-03-29 16:24:17 -0400

    initial commit

---
+++ get-function/get-function.py
@@ -0,0 +1,42 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('aws-cloud-resume')
+
+
+def get_count():
+    try:
+        response = table.query(KeyConditionExpression=Key('ID').eq('visitors'))
+        # Get the list of items or an empty list if no items
+        items = response.get('Items', [])
+        if items:  # Check if items list is not empty
+            # Retrieve the count from the first item
+            count = items[0]['visitors']
+            return int(count)
+        else:
+            return 0  # Return 0 if no items were found
+    except (ClientError, IndexError) as e:
+        return "Error occurred while retrieving count: {}".format(str(e))
+
+
+def lambda_handler(event, context):
+    try:
+        count = get_count()
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({'count': count})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
---
+++ put-function/put-function.py
@@ -0,0 +1,31 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('aws-cloud-resume')
+
+
+def lambda_handler(event, context):
+    try:
+        response = table.update_item(
+            Key={'ID': 'visitors'},
+            UpdateExpression='ADD visitors :incr',
+            ExpressionAttributeValues={':incr': 1},
+            ReturnValues="UPDATED_NEW"
+        )
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Credentials': True,
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps({'message': 'Visitor count updated successfully'})
+        }
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
